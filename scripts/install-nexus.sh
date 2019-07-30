ad978045cb40f7879e32065aac89ab1dcd705363
Abhimanyu Narwal <abhimanyunarwal@northwesternmutual.com>
2019-07-30 15:01:23 -0400

    update to use docker-ce ami

--- scripts/install-nexus.sh
+++ scripts/install-nexus.sh
@@ -1,32 +1,10 @@
 #!/bin/sh
 
-# Update yum repository
-sudo yum update -y
-
-## Install docker
-# Setup the repository
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-
-# Setup stable repository
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-#Install docker engine
-sudo yum install docker-ce docker-ce-cli containerd.io
-
-# Set docker to start automatically at boot time
-sudo systemctl enable docker
-
-# Changing user to docker
-sudo usermod -aG docker ec2-user
-
-# Start docker service
-sudo systemctl start docker
-
-# Create directory for nexus
+# Create nexus data repository
 mkdir nexus-data
 
 # Change the Data directory permission so Docker can access it
-chown -R 200 nexus-data
+sudo chown -R 200 nexus-data
 
 # Pull Nexus image
 docker pull sonatype/nexus3
